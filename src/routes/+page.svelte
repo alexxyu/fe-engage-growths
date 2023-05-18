04db7f055fea5df5ab426d1fab5b03f900410f53
Alex Yu <yu.alexanderx@gmail.com>
2023-05-17 18:22:54 -0700

    Fix logic regarding resealing advanced classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,6 @@
     final_level: number
   ) => {
     const unit = units[name];
-    const remaining_levels = final_level - current_level;
     const overall_growths = get_overall_growths(
       unit.growth_rates,
       get_class_growths(name, new_class)
@@ -24,6 +23,11 @@
 
     const class_base_stats_current = get_class(name, current_class).base_stats;
     const class_base_stats_new = get_class(name, new_class).base_stats;
+
+    let remaining_levels = final_level;
+    if (current_class === new_class && final_level >= current_level) {
+      remaining_levels = final_level - current_level;
+    }
     return overall_growths.map(
       (x, i) => class_base_stats_new[i] - class_base_stats_current[i] + (remaining_levels * x) / 100
     );
@@ -62,7 +66,8 @@
     errors = [];
     if (level_ < 1 || level_ > 40) {
       errors = [...errors, 'Level must be between 1 and 40.'];
-    } else if (level_ > 20 && class_ !== 'Thief' && class_ !== 'Dancer') {
+    }
+    if (level_ > 20 && class_ !== 'Thief' && class_ !== 'Dancer') {
       errors = [...errors, 'Only Thieves and Dancers can level past 20.'];
     }
 
@@ -76,6 +81,19 @@
     const current_class_data = get_class(name_, current_class);
     if (class_ !== current_class && current_class_data.is_base_class && current_level < 10) {
       errors = [...errors, 'Unit must be at least level 10 in a base class to change classes.'];
+    }
+    if (class_ === current_class && level_ < current_level) {
+      if (current_class_data.is_base_class) {
+        errors = [...errors, 'Unit cannot level backwards.'];
+      } else if (
+        ((class_ === 'Thief' || class_ === 'Dancer') && current_level < 40) ||
+        current_level < 20
+      ) {
+        errors = [
+          ...errors,
+          'Make sure your unit is at the max level before resealing into the same class.'
+        ];
+      }
     }
 
     if (errors.length > 0) {
@@ -126,6 +144,17 @@
         errors = [
           ...errors,
           'Deleting this entry is invalid since units in base classes must be at least level 10 to change classes.'
+        ];
+      }
+    } else if (
+      next_entry &&
+      next_entry.class_name === curr_entry.class_name &&
+      !curr_class_data.is_base_class
+    ) {
+      if (next_entry.final_level < curr_entry.final_level) {
+        errors = [
+          ...errors,
+          'Deleting this entry is invalid because units must hit their max level to reseal into the same advanced class.'
         ];
       }
     }
@@ -212,7 +241,7 @@
                 class="btn btn-sm btn-outline btn-primary"
                 disabled={data[name].length >= 10}
               >
-                Add promotion
+                Add Entry
               </button>
             </form>
             <table class="table">
